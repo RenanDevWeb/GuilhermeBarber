e1d36a8059f084ad60696a1a408cc7c7f2bbdaaa
Renan <renansalmazio@gmail.com>
2021-08-25 19:52:40 -0300

    finalizando detalhes do site

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+//      const menuu = document.getElementById('box-menu')
+
+   
+//  function showMenu(){
+//        if(menuu.classList.contains("box-menu")){
+//              menuu.classList.add("box-menu-show")
+//              menuu.classList.add("box-menu")
+//     }else
+//      if(menuu.classList.contains("box-menu-show")){
+//         menuu.classList.add("box-menu")
+//         console.log("2")
+//     }
+         
+        
+    //   let title = document.getElementById("title").innerText
+    //         const count = 1
+    //        const tituloSeparado = title.split("")
+    //         title.innerHTML = ''
+    //         tituloSeparado.forEach((letra,i) => {
+    //             setTimeout(() => {
+    //                 console.log(letra)
+    //             }, 3000 + i + count )
+    //     });
+       
+
+
+
